a9dd0ec18c594df39bcefc2fc3ee945b4618b355
Njfeld <87391920+Njfeld@users.noreply.github.com>
2025-07-20 07:56:26 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,15 +1,28 @@
-function updateText(){
-    document.getElementById("heading").innerHTML = "JavaScrip example";
+// Function to update the text of the heading
+function updateText() {
+    // Gets the HTML element with the ID "heading"
+    let heading = document.getElementById("heading");
+    // Changes the inner HTML (text content) of the heading
+    heading.innerHTML = "Text has been changed!";
 }
 
-function rollDice(){
-    let diceRoll = Math.floor(Math.random() * 6) + 1;
-    let message = `You rolled a ${diceRoll}!`;
-    displayMessage(message);
-}
-function displayMessage(message) {
+// Function to simulate rolling a six-sided die
+function rollDice() {
+    // Generates a random number between 0 and 1
+
+    let dieRoll = Math.floor(Math.random() * 6) + 1; 
+    // Gets the HTML element where the game output will be displayed
     let outputDiv = document.getElementById("gameOutput");
-    outputDiv.innerHTML = message;
+
+    // Sets the initial text to display the result of the die roll
+    outputDiv.innerHTML = "You rolled a " + dieRoll + ".<br>";
+
+    // Checks if the die roll is an even number
+    if (dieRoll % 2 === 0) {
+        // output indicating the roll was even
+        outputDiv.innerHTML += "That's an even number!";
+    } else {
+        // output indicating the roll was odd
+        outputDiv.innerHTML += "That's an odd number!";
+    }
 }
-// this took me forever and i am not good at commenting 
-// keeps the example text changer adds function to put button roll dice and then function to display results
