6d93756c085286085d439a8db1a0173abf669a3a
Njfeld <neufeldjustinc@gmail.com>
2025-07-25 04:16:52 -0500

    scripts update

--- strings.js
+++ strings.js
@@ -13,5 +13,32 @@ console.log("firstName="+ FirstName);
 console.log("LastName="+ LastName);
 console.log("ZipCode="+ ZipCode);
 //  keeps from submit 
+// create a var to conbime first and last name 
+// debug space"""
+let fullName = FirstName + " " + LastName;
+
+// short cut to div in html
+
+let divMessage = document.getElementById("divMessage")
+
+
+// see how long name if full 20 caricters or less
+
+if(fullName.length == 1|| fullName.length > 20){
+    // user notify error 
+    divMessage.innerHTML ="fill out form will fail if name is longer than 19 characters"
+
+}
+// zip must be five numbers 
+
+else if (ZipCode.length !=5){
+    divMessage.innerHTML ="Enter a zip code"
+}
+// else display the mssg all imput is good
+else( 
+    divMessage.innerHTML ="You did it!"+" "+fullName+ " " +"you filled out a form."+"the secret word is bureaucracy. "
+
+)
+
     return false;
 }
