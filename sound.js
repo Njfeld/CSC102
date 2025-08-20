0643307f7c275fd35e596eeeb32146093d08281e
Njfeld <neufeldjustinc@gmail.com>
2025-08-20 02:41:25 -0500

    6.1f

---
+++ sound.js
@@ -0,0 +1,30 @@
+// sound management 
+
+function addSound() {
+// Code to add sound
+    let btnPlay = document.getElementById("btnPlay");
+    let btnPause = document.getElementById("btnPause");
+    // audio element 
+    let audduck = document.createElement("audio");
+
+    // aud id
+    audduck.setAttribute("id", "audduck");
+    audduck.setAttribute("src", "mallard.mp3");
+    // add to page
+    document.body.appendChild(audduck);
+    // show play and pause buttons
+    btnPlay.hidden = false;
+    btnPause.hidden = false;
+}
+
+function playSound() {
+    // Code to play sound
+    let audduck = document.getElementById("audduck");
+    audduck.play();
+}
+
+function pauseSound() {
+    // Code to pause sound
+    let audduck = document.getElementById("audduck");
+    audduck.pause();
+}
