b98b8eaa38b4b6d4b51f716a0c66fb66f2a4cce3
Njfeld <neufeldjustinc@gmail.com>
2025-08-24 03:35:28 -0500

    7.1

--- table.js
+++ table.js
@@ -20,16 +20,15 @@ for (let i = 0; i < animalData.length; i++) {
     let tdFood = document.createElement("td");
     let tdAmount = document.createElement("td");
 
-    // Set the text content for each cell from the current animal object.
     tdAnimal.textContent = animalData[i].animal;
     tdFood.textContent = animalData[i].food;
     tdAmount.textContent = animalData[i].amount;
 
-    // Append the new cells to the row. This must happen before the row is added to the table.
+    // Append the new cells to the row. 
     row.appendChild(tdAnimal);
     row.appendChild(tdFood);
     row.appendChild(tdAmount);
 
-    // Now, append the complete row to the table body.
+    //append the complete row to the table body.
     myAnimals.appendChild(row);
 }
